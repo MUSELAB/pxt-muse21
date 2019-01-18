359486a49ec17ea327fb533a0403ca4a2e3077af
Daniel <daniel.bluhm@muselab.cc>
2019-01-18 11:01:27 +0800

    Adding Ultrasonic Sensoring

--- muse21.ts
+++ muse21.ts
@@ -63,6 +63,15 @@ namespace Muse21 {
 
     }
 
+    export enum PingUnit {
+        //% block="μs"
+        MicroSeconds,
+        //% block="cm"
+        Centimeters,
+        //% block="inches"
+        Inches
+    }
+
     //% blockId="turnoutput"
     //% block="Set Muselab %muselabtype| pin %pin| to %onoff"
     //% weight=65
@@ -210,6 +219,37 @@ namespace Muse21 {
     export function ReadSoundSensor(ports: inputPorts): number {
         let temp = parseInt(ports.toString());
         return pins.map(Math.abs(pins.analogReadPin(temp) - 512),0,512,0,1023);
+    }
+
+
+    /**
+     * Send a ping and get the echo time (in microseconds) as a result
+     * @param trig tigger pin
+     * @param echo echo pin
+     * @param unit desired conversion unit
+     * @param maxCmDistance maximum distance in centimeters (default is 500)
+     */
+    //%subcategory=More
+    //% blockId=Ultrasonic_Sensor
+    //%block="ping trig %trig|echo %echo|unit %unit"
+	//% weight=13
+    export function ping(trig: DigitalPin, echo: DigitalPin, unit: PingUnit, maxCmDistance = 500): number {
+        // send pulse
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(trig, 0);
+
+        // read pulse
+        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+
+        switch (unit) {
+            case PingUnit.Centimeters: return Math.idiv(d, 58);
+            case PingUnit.Inches: return Math.idiv(d, 148);
+            default: return d ;
+        }
     }
 
 }
